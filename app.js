db0431c0c7ab3509c7343964968b6ea98e7af9a9
clicksun <clicksun@gmail.com>
2017-07-31 17:14:32 -0500

    initial ground work

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const volleyball = require('volleyball');
+const PORT = 3000;
+
+// app.use(function (req, res, next) {
+//   console.log(req.method, req.url, res.statusCode);
+//   next();
+// });
+
+app.use(volleyball);
+
+app.get('/', function(req, res) {
+  res.send('Welcome to twitter.js!');
+});
+
+app.listen(PORT, function(req, res) {
+  console.log('server listening');
+});
