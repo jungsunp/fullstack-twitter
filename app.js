f9b9012814453584f34b60e9eb9d7f18b25aca0e
clicksun <clicksun@gmail.com>
2017-07-31 17:38:00 -0500

    more project work

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const volleyball = require('volleyball');
+const nunjucks = require('nunjucks');
 const PORT = 3000;
 
 // app.use(function (req, res, next) {
@@ -9,6 +10,11 @@ const PORT = 3000;
 // });
 
 app.use(volleyball);
+nunjucks.configure('./views');
+console.log(nunjucks.render('index.html', {
+  title: 'An Example',
+  people: [{name: 'Gandolf'}, {name:'Frodo'}, {name:'Hermione'}, {name:'Harry Potter'}]
+}));
 
 app.get('/', function(req, res) {
   res.send('Welcome to twitter.js!');
@@ -17,3 +23,6 @@ app.get('/', function(req, res) {
 app.listen(PORT, function(req, res) {
   console.log('server listening at http://localhost:' + PORT + '/');
 });
+
+
+
