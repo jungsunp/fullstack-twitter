3b4d69ae78bd9f62696f69c6d2e8eeaca724ab49
clicksun <clicksun@gmail.com>
2017-08-01 09:17:31 -0500

    Render index.html

--- app.js
+++ app.js
@@ -3,21 +3,27 @@ const app = express();
 const volleyball = require('volleyball');
 const nunjucks = require('nunjucks');
 const PORT = 3000;
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 
 // app.use(function (req, res, next) {
 //   console.log(req.method, req.url, res.statusCode);
 //   next();
 // });
 
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+
 app.use(volleyball);
-nunjucks.configure('./views');
-console.log(nunjucks.render('index.html', {
-  title: 'An Example',
-  people: [{name: 'Gandolf'}, {name:'Frodo'}, {name:'Hermione'}, {name:'Harry Potter'}]
-}));
+
+nunjucks.configure('views', { noCache: true });
+// console.log(nunjucks.render('index.html', {
+//   title: 'An Example',
+//   people: [{name: 'Gandolf'}, {name: 'Frodo'}, {name: 'Hermione'}, {name: 'Harry Potter'}]
+// }));
 
 app.get('/', function(req, res) {
-  res.send('Welcome to twitter.js!');
+  // res.send('Welcome to twitter.js!');
+  res.render( 'index', {title: 'Hall of Fame', people: people} );
 });
 
 app.listen(PORT, function(req, res) {
