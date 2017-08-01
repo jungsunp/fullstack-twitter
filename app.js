06a6a4dc9a376547c78a2b403b36f60c172e4392
clicksun <clicksun@gmail.com>
2017-08-01 10:58:37 -0500

    Add basic layout

--- app.js
+++ app.js
@@ -3,29 +3,19 @@ const app = express();
 const volleyball = require('volleyball');
 const nunjucks = require('nunjucks');
 const PORT = 3000;
-const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-
-// app.use(function (req, res, next) {
-//   console.log(req.method, req.url, res.statusCode);
-//   next();
-// });
+const routes = require('./routes');
 
 app.set('view engine', 'html');
+
 app.engine('html', nunjucks.render);
 
 app.use(volleyball);
 
 nunjucks.configure('views', { noCache: true });
-// console.log(nunjucks.render('index.html', {
-//   title: 'An Example',
-//   people: [{name: 'Gandolf'}, {name: 'Frodo'}, {name: 'Hermione'}, {name: 'Harry Potter'}]
-// }));
 
-// app.get('/', function(req, res) {
-//   // res.send('Welcome to twitter.js!');
-//   res.render( 'index', {title: 'Hall of Fame', people: people} );
-// });
+app.use('/', routes);
 
 app.listen(PORT, function(req, res) {
   console.log('server listening at http://localhost:' + PORT + '/');
 });
+
