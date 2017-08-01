435a982cbb62420254ee9da21f5938d3002acdf6
clicksun <clicksun@gmail.com>
2017-08-01 11:42:21 -0500

    Add tweet list pages for individual users

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();  // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
-const path = require('path');
 
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
   res.render( 'index', { tweets: tweets } );
 });
 
-router.use(express.static('public'));
+router.get('/users/:name', function(req, res) {
+  let name = req.params.name;
+  let list = tweetBank.find( {name: name} );
+  res.render( 'index', { tweets: list } );
+});
 
-// router.get('/stylesheets/style.css', function (req, res){
-//   res.sendFile(path.resolve('./public/stylesheets/style.css'));
-// });
+router.use(express.static('public'));
 
 module.exports = router;
